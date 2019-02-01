3939362c6076a9de564587368e7d67aeea4a380f
Maarten Visscher <mail@maartenvisscher.nl>
2019-02-01 01:04:38 +0100

    Update association urls

--- UserDetails/urls_association.py
+++ UserDetails/urls_association.py
@@ -1,15 +1,11 @@
 from django.urls import path
-from . import views_association
-from CreditManagement.views import TransactionAddView
 
+from CreditManagement.views import TransactionAddView
+from .views_association import CreditsOverview, MembersOverview, MembersEditView
 
 urlpatterns = [
-    path('transactions', views_association.CreditsOverview.as_view(), name='association_credits'),
-    path('transactions/<int:page>', views_association.CreditsOverview.as_view(), name='association_credits'),
-    path('transactions/add', TransactionAddView.as_view(), name='transaction_add'),
-    path('members', views_association.MembersOverview.as_view(), name='association_members'),
-    path('members/<int:page>', views_association.MembersOverview.as_view(), name='association_members'),
-    path('members/edit', views_association.MembersEditView.as_view(), name='association_members_edit'),
-    path('members/edit/<int:page>', views_association.MembersEditView.as_view(), name='association_members_edit'),
-
+    path('transactions/', CreditsOverview.as_view(), name='association_credits'),
+    path('transactions/add/', TransactionAddView.as_view(), name='transaction_add'),
+    path('members/', MembersOverview.as_view(), name='association_members'),
+    path('members/edit/', MembersEditView.as_view(), name='association_members_edit'),
 ]
