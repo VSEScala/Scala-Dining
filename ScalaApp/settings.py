b7db12278cfcd560f5076597d4c57b29f76e5120
Maarten Visscher <mail@maartenvisscher.nl>
2019-02-04 03:05:54 +0100

    Add Q OpenID Connect

--- ScalaApp/settings.py
+++ ScalaApp/settings.py
@@ -44,6 +44,8 @@ INSTALLED_APPS = [
 
     'widget_tweaks',
 
+    'mozilla_django_oidc',
+
     'UserDetails.apps.UserDetailsConfig',
     'Dining.apps.DiningConfig',
     'CreditManagement.apps.CreditManagementConfig',
@@ -81,10 +83,9 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'ScalaApp.wsgi.application'
 
-LOGIN_REDIRECT_URL = 'index'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
-# (dubbelop)
-# STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static_total")
 
@@ -163,3 +164,34 @@ INTERNAL_IPS = ['127.0.0.1']
 
 # Include Scala settings
 from ScalaApp.scala_settings import *
+
+# Enable OpenID Connect
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'mozilla_django_oidc.auth.OIDCAuthenticationBackend',
+]
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'mozilla_django_oidc': {
+            'handlers': ['console'],
+            'level': 'DEBUG'
+        },
+    },
+}
+
+# Quadrivium OpenID Connect settings
+OIDC_RP_CLIENT_ID = 'scaladining'
+OIDC_RP_CLIENT_SECRET = '707e5260-39b9-4bd9-9d4d-da5ec5081fd1'
+OIDC_OP_AUTHORIZATION_ENDPOINT = "https://keycloak.esmgquadrivium.nl/auth/realms/esmgquadrivium/protocol/openid-connect/auth"
+OIDC_OP_TOKEN_ENDPOINT = "https://keycloak.esmgquadrivium.nl/auth/realms/esmgquadrivium/protocol/openid-connect/token"
+OIDC_OP_USER_ENDPOINT = "https://keycloak.esmgquadrivium.nl/auth/realms/esmgquadrivium/protocol/openid-connect/userinfo"
+OIDC_RP_SIGN_ALGO = "RS256"
+OIDC_OP_JWKS_ENDPOINT = "https://keycloak.esmgquadrivium.nl/auth/realms/esmgquadrivium/protocol/openid-connect/certs"
