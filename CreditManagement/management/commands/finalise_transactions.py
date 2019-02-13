b53c1eee798067f34b18af1e365f86a4359b96d9
Wouter <wouter@fam-roos.com>
2019-02-13 11:35:03 +0100

    added finalisation method for expired transactions to manage.py

---
+++ CreditManagement/management/commands/finalise_transactions.py
@@ -0,0 +1,10 @@
+from django.core.management.base import BaseCommand
+from CreditManagement.models import AbstractPendingTransaction
+
+class Command(BaseCommand):
+    help = 'Finalises all expired transactions'
+
+    def handle(self, *args, **options):
+        results = AbstractPendingTransaction.finalise_all_expired()
+        for transaction in results:
+            self.stdout.write(self.style.SUCCESS('Successfully finalised "%s"' % transaction))
