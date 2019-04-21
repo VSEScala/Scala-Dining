9844b882be748a83a6f00f63f2fd0750bae2d468
Wouter <wouter@fam-roos.com>
2019-04-21 16:19:54 +0200

    Rewrote paginator layout to display multiple pages instead

---
+++ General/templatetags/computation_tags.py
@@ -0,0 +1,19 @@
+from django import template
+
+
+register = template.Library()
+
+
+@register.filter
+def subtract(value, arg):
+    return value - arg
+
+
+@register.filter
+def minimum(value, arg):
+    return min(value, arg)
+
+
+@register.filter
+def maximum(value, arg):
+    return min(value, arg)
