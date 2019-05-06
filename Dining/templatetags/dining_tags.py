641485f0e40717ca215721a33e662167a8fe0db8
Wouter <wouter@fam-roos.com>
2019-05-06 23:32:44 +0200

    Removed unnecessary name declaration

--- Dining/templatetags/dining_tags.py
+++ Dining/templatetags/dining_tags.py
@@ -5,18 +5,18 @@ from Dining.models import DiningEntry
 register = template.Library()
 
 
-@register.filter(name='can_join_slot')
+@register.filter
 def can_join_slot(slot, user):
     # Try creating an entry
     return slot.can_add_diners(user, check_for_self=True)
 
 
-@register.filter(name='is_on_slot')
+@register.filter
 def is_on_slot(slot, user):
     return slot.internal_dining_entries().filter(user=user).exists()
 
 
-@register.filter(name='has_paid')
+@register.filter
 def has_paid(slot, user):
     try:
         entry = slot.internal_dining_entries().get(user=user)
