1b6ba3343991b4fbd786008f747c1c89956a6ad0
Maarten Visscher <mail@maartenvisscher.nl>
2019-05-10 20:22:43 +0200

    Expand Q min balance exception

--- Dining/tests/testforms.py
+++ Dining/tests/testforms.py
@@ -1,34 +1,21 @@
-from datetime import time, date, timedelta, datetime
+from datetime import time, timedelta
 from decimal import Decimal
 
-from django.core.exceptions import NON_FIELD_ERRORS
 from django.test import TestCase
 from django.utils import timezone
 
 from CreditManagement.models import FixedTransaction
-from Dining.forms import CreateSlotForm, DiningEntryExternalCreateForm, DiningEntryUserCreateForm
+from Dining.forms import CreateSlotForm
+from Dining.models import DiningList
 from UserDetails.models import Association, User, UserMembership
-from Dining.models import DiningList, DiningEntryExternal
 
 
 class CreateSlotFormTestCase(TestCase):
-    @classmethod
-    def setUpTestData(cls):
-        """
-        Do not modify the objects created here in tests, see:
-        https://docs.djangoproject.com/en/2.1/topics/testing/tools/#django.test.TestCase.setUpTestData
-        """
-        cls.association1 = Association.objects.create(name="Quadrivium")
-        cls.association2 = Association.objects.create(name="Knights")
-        cls.association3 = Association.objects.create(name="Scala")
-        cls.user1 = User.objects.create_user('jan')
-        # cls.user2 = User.objects.create_user('klaas')
-        cls.user1_assoc1 = UserMembership.objects.create(related_user=cls.user1, association=cls.association1,
-                                                         is_verified=True)
-        cls.user1_assoc2 = UserMembership.objects.create(related_user=cls.user1, association=cls.association2,
-                                                         is_verified=True)
-
     def setUp(self):
+        self.association1 = Association.objects.create(name="Quadrivium")
+        self.user1 = User.objects.create_user('jan')
+        UserMembership.objects.create(related_user=self.user1, association=self.association1, is_verified=True,
+                                      verified_on=timezone.now())
         # Date two days in the future
         self.dining_date = timezone.now().date() + timedelta(days=2)
         self.form_data = {'dish': 'Kwark', 'association': str(self.association1.pk), 'max_diners': '18',
@@ -49,29 +36,47 @@ class CreateSlotFormTestCase(TestCase):
         self.assertEqual(self.dining_date, dining_list.date)
 
     def test_invalid_association(self):
-        """
-        Tests using an association which the user is not a member of.
+        """Tests using an association which the user is not a member of
 
-        Association 3 with user 1.
+        NOTE: when there is 1 available association, the form sets the association field to disabled. This means that
+        the value that is sent with the form is totally ignored in favor of the initial value of the field. Therefore
+        setting a different invalid association in the form data results in the form data actually being valid, but it
+        does not use this association, instead it uses the other association which the user is actually a member of.
+
+        Source: https://docs.djangoproject.com/en/2.2/ref/forms/fields/#disabled
         """
-        form_data = {'dish': 'Boter', 'association': str(self.association3.pk), 'max_diners': '20',
+        association = Association.objects.create(name='Knights')
+        form_data = {'dish': 'Boter', 'association': str(association.pk), 'max_diners': '20',
                      'serve_time': '18:00'}
         form = CreateSlotForm(form_data, instance=DiningList(claimed_by=self.user1, date=self.dining_date))
+        self.assertTrue(form.is_valid())
+        # Check that the actual association is not Knights but Quadrivium
+        self.assertEqual(self.association1, form.instance.association)
+
+    def test_association_unique_for_date(self):
+        """Test that there can be only one dining slot for an association for each date"""
+        # Save one dining list
+        self.form.save()
+
+        # Try creating another one with same association
+        dl = DiningList(claimed_by=self.user1, date=self.dining_date)
+        data = {'dish': 'Kwark', 'association': str(self.association1.pk), 'max_diners': '18',
+                'serve_time': '17:00'}
+        form = CreateSlotForm(data, instance=dl)
         self.assertFalse(form.is_valid())
-        self.assertTrue(form.has_error('association', 'invalid_choice'))
+        self.assertTrue(form.has_error('association'))
 
-    def test_occupied_association(self):
-        """
-        Creating a dining list on a date which is already occupied for your association.
-        """
-        # Todo: don't know why this test fails
-        pass
-        # DiningList.objects.create(date=self.dining_date, association=self.association1)
-        # form_data = {'dish': '', 'association': self.association1.pk, 'max_diners': 20, 'serve_time': time(18, 00)}
-        # form = CreateSlotForm(self.user1, self.dining_date, form_data)
-        # self.assertFalse(form.is_valid())
-        # self.assertTrue(form.has_error('association'))
+    def test_insufficient_balance(self):
+        FixedTransaction.objects.create(source_user=self.user1, amount=Decimal('99'))
+        self.assertFalse(self.form.is_valid())
 
+    def test_insufficient_balance_exception(self):
+        FixedTransaction.objects.create(source_user=self.user1, amount=Decimal('99'))
+        # Make user member of another association that has the exception
+        association = Association.objects.create(name='Q', has_min_exception=True)
+        UserMembership.objects.create(related_user=self.user1, association=association, is_verified=True,
+                                      verified_on=timezone.now())
+        self.assertTrue(self.form.is_valid())
 
     def test_serve_time_too_late(self):
         # Actually tests a different class, but put here for convenience, to test it via the CreateSlotForm class
@@ -82,4 +87,3 @@ class CreateSlotFormTestCase(TestCase):
         # Actually tests a different class, but put here for convenience, to test it via the CreateSlotForm class
         self.form_data['serve_time'] = '11:00'
         self.assertFalse(self.form.is_valid())
-
