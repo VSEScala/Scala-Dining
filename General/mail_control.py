16dad2393a20602f04cabcd7bda88574e0f9e3de
Wouter <wouter@fam-roos.com>
2019-08-08 00:07:30 +0200

    Removed class from mail control

--- General/mail_control.py
+++ General/mail_control.py
@@ -2,58 +2,53 @@ from django.core.mail import EmailMultiAlternatives, get_connection
 from django.template.loader import get_template, TemplateDoesNotExist
 
 
-class EmailTemplateMessage(EmailMultiAlternatives):
-    """
-    Renders emails from a given template
-    """
+def _render_and_send_mail(*args, txt_template=None, html_template=None, context_data={}, fail_silently=False, **kwargs):
+    # Set up the Email template with the txt_template
+    mail_obj = EmailMultiAlternatives(*args, **kwargs, body=txt_template.render(context_data))
 
-    def __init__(self, *args, txt_template=None, html_template=None, context_data={}, **kwargs):
-        # remove body and from_email from kwarg arguments
-        kwargs.pop('body', None)
+    # Set up the html content in the mail
+    if html_template is not None:
+        content_html = html_template.render(context_data)
+        mail_obj.attach_alternative(content_html, "text/html")
 
-        # Set up the Email template with the txt_template
-        super(EmailTemplateMessage, self).__init__(*args, **kwargs,
-                                                   body=txt_template.render(context_data))
-        # Set up the html content in the mail
-        if html_template is not None:
-            content_html = html_template.render(context_data)
-            self.attach_alternative(content_html, "text/html")
+    # Send the mail
+    mail_obj.send(fail_silently=fail_silently)
 
 
-def get_mail_templates(full_template_name):
+def _get_mail_templates(full_template_name):
     try:
         return get_template(full_template_name, using='EmailTemplates')
     except TemplateDoesNotExist:
             return None
 
 
-def send_templated_mail(subject=None, template_name=None, context_data={}, recipient=None, fail_silently=False, **kwargs):
+def send_templated_mail(subject=None, template_name=None, context_data={}, recipient=None, **kwargs):
     if recipient is None:
         raise KeyError("No email target given. Please define the recipient")
 
     context_data['user'] = recipient
     to = [recipient.email]
 
-    EmailTemplateMessage(subject=subject,
-                         txt_template=get_mail_templates(template_name+".txt"),
-                         context_data=get_mail_templates(template_name+".html"),
-                         to=to, **kwargs).send(fail_silently=fail_silently)
+    _render_and_send_mail(subject=subject,
+                          txt_template=_get_mail_templates(template_name+".txt"),
+                          html_template=_get_mail_templates(template_name+".html"),
+                          context_data=context_data,
+                          to=to, **kwargs)
 
 
-def send_templated_mass_mail(subject=None, template_name=None, context_data={}, recipients=None, fail_silently=False, **kwargs):
+def send_templated_mass_mail(subject=None, template_name=None, context_data={}, recipients=None, **kwargs):
     """
     Send a mass mail to all recipients with an EmailTemplateMessage as the message created
     :param subject: The email subject or header
     :param template_name: The name of the template
     :param context_data: the context data for the mail
     :param recipients: The queryset of users
-    :param fail_silently: Whether an error needs to be created when sending fails
     :param kwargs: additional EmailTemplateMessage arguments
     """
 
     # Get the templates
-    txt_template = get_mail_templates(template_name+".txt")
-    html_template = get_mail_templates(template_name+".html")
+    txt_template = _get_mail_templates(template_name+".txt")
+    html_template = _get_mail_templates(template_name+".html")
 
     # Open the connection
     connection = get_connection()
@@ -64,13 +59,11 @@ def send_templated_mass_mail(subject=None, template_name=None, context_data={}, 
         context_data['user'] = recipient
         to = [recipient.email]
 
-        EmailTemplateMessage(subject=subject,
-                             txt_template=txt_template,
-                             html_template=html_template,
-                             context_data=context_data,
-                             to=to, **kwargs)\
-            .send(fail_silently=fail_silently)
-
+        _render_and_send_mail(subject=subject,
+                              txt_template=txt_template,
+                              html_template=html_template,
+                              context_data=context_data,
+                              to=to, **kwargs)
     connection.close()
 
 
