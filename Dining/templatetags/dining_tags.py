3c8c409a41f2932f84f522572ec4030d428340c1
Maarten Visscher <mail@maartenvisscher.nl>
2019-08-07 21:08:20 +0200

    Only display first names for multiple owners

--- Dining/templatetags/dining_tags.py
+++ Dining/templatetags/dining_tags.py
@@ -118,3 +118,26 @@ def cant_create_dining_list_reason(user: User, date: datetime.date) -> Optional[
         return "you already have a dining list for this day"
 
     return None
+
+
+@register.filter
+def short_owners_string(dining_list: DiningList) -> str:
+    """Returns the names of all owners for display in short form.
+
+    For a larger number of owners, only the first name will be included.
+
+    Returns:
+        Names of the owners separated by ',' and 'and'
+    """
+    owners = dining_list.owners.all()
+
+    if len(owners) > 1:
+        names = [o.first_name for o in owners]
+    else:
+        names = [o.get_full_name() for o in owners]
+
+    if len(names) >= 2:
+        # Join all but last names by ',' and put 'and' in front of last name
+        commaseparated = ', '.join(names[0:-1])
+        return '{} and {}'.format(commaseparated, names[-1])
+    return names[0]
