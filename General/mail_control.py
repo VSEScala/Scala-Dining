b15146d1dedca4fad8d97b1af8273de5cc8aa794
Wouter <wouter@fam-roos.com>
2019-08-07 23:52:51 +0200

    Simplified mail sending code by removing obvious error catching

--- General/mail_control.py
+++ General/mail_control.py
@@ -1,54 +1,30 @@
 from django.core.mail import EmailMultiAlternatives, get_connection
 from django.template.loader import get_template, TemplateDoesNotExist
-from django.conf import settings
 
 
 class EmailTemplateMessage(EmailMultiAlternatives):
     """
     Renders emails from a given template
     """
-    mail_source = settings.DEFAULT_FROM_EMAIL
 
-    def __init__(self, *args, template_name=None, txt_template=None, html_template=None, context_data={}, **kwargs):
-        # Check if the templates are given properly, if not, recompute them.
-        if template_name is not None:
-            if txt_template is not None:
-                raise KeyError("Both a template and template name were given, only one of which was expected")
-            txt_template, html_template = self.get_txt_html_templates(template_name)
-        elif txt_template is None:
-            raise KeyError("Template name is not defined")
-
+    def __init__(self, *args, txt_template=None, html_template=None, context_data={}, **kwargs):
         # remove body and from_email from kwarg arguments
         kwargs.pop('body', None)
-        kwargs.pop('from_email', None)
 
         # Set up the Email template with the txt_template
         super(EmailTemplateMessage, self).__init__(*args, **kwargs,
-                                                   body=txt_template.render(context_data),
-                                                   from_email=self.mail_source)
+                                                   body=txt_template.render(context_data))
         # Set up the html content in the mail
         if html_template is not None:
             content_html = html_template.render(context_data)
             self.attach_alternative(content_html, "text/html")
 
-    @classmethod
-    def get_txt_html_templates(cls, template_name):
-        # Trim accidental file extensions
-        for appendix in [".txt", ".html"]:
-            if template_name.endswith(appendix):
-                template_name = template_name[:, -len(appendix)]
-        # Get the plain txt template
-        try:
-            txt_template = get_template(template_name+'.txt', using='EmailTemplates')
-        except TemplateDoesNotExist:
-            raise KeyError("{template}.txt does not exist, a plain version must exist".format(template=template_name))
 
-        # Get the advanced html template
-        try:
-            html_template = get_template(template_name+'.html', using='EmailTemplates')
-            return txt_template, html_template
-        except TemplateDoesNotExist:
-            return txt_template, None
+def get_mail_templates(full_template_name):
+    try:
+        return get_template(full_template_name, using='EmailTemplates')
+    except TemplateDoesNotExist:
+            return None
 
 
 def send_templated_mail(subject=None, template_name=None, context_data={}, recipient=None, fail_silently=False, **kwargs):
@@ -59,8 +35,8 @@ def send_templated_mail(subject=None, template_name=None, context_data={}, recip
     to = [recipient.email]
 
     EmailTemplateMessage(subject=subject,
-                         template_name=template_name,
-                         context_data=context_data,
+                         txt_template=get_mail_templates(template_name+".txt"),
+                         context_data=get_mail_templates(template_name+".html"),
                          to=to, **kwargs).send(fail_silently=fail_silently)
 
 
@@ -76,7 +52,8 @@ def send_templated_mass_mail(subject=None, template_name=None, context_data={}, 
     """
 
     # Get the templates
-    txt_template, html_template = EmailTemplateMessage.get_txt_html_templates(template_name)
+    txt_template = get_mail_templates(template_name+".txt")
+    html_template = get_mail_templates(template_name+".html")
 
     # Open the connection
     connection = get_connection()
